8e0f750bd21b8c4c6bc29b99f16bf4b8a44e3b3d
Pablo Bozzolo <boctulus@gmail.com>
2021-07-29 22:45:34 -0300

    .

--- reactor.php
+++ reactor.php
@@ -42,57 +42,78 @@ if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', 
 //		return;
 //}
 
+/*
+	https://www.mootpoint.org/blog/woocommerce-hook-product-updated-added/
+*/
+
+
 $action = null;
 
-add_action( 'woocommerce_update_product', 'sync_on_product_update', 11, 1 );
+class Reactor 
+{
+	protected $action = null;
 
-function sync_on_product_update( $product_id ) {
-	$action = 'edit';
-    $product = wc_get_product( $product_id );
-    
-	dd($product, "Producto - ". $action);
-	//Files::dump($product, 'product_edit');
-	exit; //
-}
+	function __construct()
+	{
+		add_action( 'woocommerce_update_product', [$this, 'sync_on_product_update'], 11, 1 );
+		add_action('added_post_meta', [$this, 'sync_on_product_add'], 10, 4 );
+	}	
 
-
-// este hook reacciona a productos agregados o modificados
-add_action('added_post_meta', 'sync_on_product_add', 10, 4 );
-
-function sync_on_product_add( $meta_id, $post_id, $meta_key, $meta_value ) {  
-	if ( get_post_type( $post_id ) == 'product' ) { // we've been editing a product
-		//dd($meta_key, 'META KEY');  
+	private function sync_on_product_update( $product_id ) {
+		$action = 'edit';
+		$product = wc_get_product( $product_id );
 		
-		/*
-			$meta_key == 
-			
-			_wp_trash_meta_status  => es borrado
-			_wp_old_slug => restaurado
-			_stock => editado
-		*/
-
-		// si ya lo cogió el otro hook
-		if ($meta_key == '_stock'){
-			return;
-		}
-
-		switch ($meta_key){
-			case '_wp_trash_meta_status': 
-				$action = 'trashed';
-				break;
-			case '_wp_old_slug':
-				$action = 'restored';
-				break;
-		}
-
-
-		$product = wc_get_product( $post_id );			
 		dd($product, "Producto - ". $action);
 
-		//Files::dump($product, 'product_add');
-		exit; //
+		export_wp(); /////////
+
+		//Files::dump($product, 'product_edit');
+		#exit; //
 	}
- 
+
+	function sync_on_product_add( $meta_id, $post_id, $meta_key, $meta_value ) {  
+		if (get_post_type( $post_id ) == 'product') { 
+			//dd($meta_key, 'META KEY');  
+
+			/*
+				$meta_key == 
+				
+				_wp_trash_meta_status  => es borrado
+				_wp_old_slug => restaurado
+				_stock => editado
+			*/
+
+			// si ya lo cogió el otro hook
+			if ($this->action == 'edit'){
+				return;
+			}
+
+			switch ($meta_key){
+				case '_wp_trash_meta_status': 
+					$this->action = 'trash';
+					break;
+				case '_wp_old_slug':
+					$this->action = 'restore';
+					break;
+				case '_stock':
+					$this->action = 'edit';
+					break;
+				// creación
+				default:
+					$this->action = 'create';
+			}
+
+			$product = wc_get_product( $post_id );			
+			dd($product, "Producto - ". $this->action);
+
+			export_wp(); /////////
+
+			#Files::dump($product, 'product_add');
+			#exit; //
+		}
+	
+	}
 }
 
 
+$reactor = new Reactor();
